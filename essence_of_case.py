c6599eab38e17ef10385b6a92644174bb1602c55
nik1kit <49343348+nik1kit@users.noreply.github.com>
2024-07-15 01:13:42 +0300

    Update essence_of_case.py
    Косметические изменения

--- essence_of_case.py
+++ essence_of_case.py
@@ -4,7 +4,7 @@ from common import ESSENCE_OF_CASE
 def essence_of_case(html_code):
     soup = html_code.find("dl", class_="b-iblock b-iblock_lightgrey").find("span")
     if soup != None:
-        ESSENCE_OF_CASE.append(soup.text.rstrip().lstrip())
+        ESSENCE_OF_CASE.append(soup.text.strip())
     else:
         ESSENCE_OF_CASE.append(f"-")
 
