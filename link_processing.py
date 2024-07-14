ac53652183f5d6389b9b86d05072f7e613b497f1
nik1kit <49343348+nik1kit@users.noreply.github.com>
2024-07-15 01:18:42 +0300

    Update link_processing.py
    Добавлен прокси

--- link_processing.py
+++ link_processing.py
@@ -1,26 +1,47 @@
 from bs4 import BeautifulSoup
 from essence_of_case import essence_of_case
+from common import IS_PROXIES
 
 from selenium import webdriver
 import time
 from common import PLAINTIFFS, DEFENDANTS, THIRDS, OTHERS
 
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+}
+
+if IS_PROXIES:
+    proxies = {
+        'https': "http://145.255.30.241:8088"
+    }
+else:
+    proxies = {}
 
 def link_processing(links, session):
     for link in links:
-        time.sleep(5)
-        response = session.get(link)
+        time.sleep(3)
+        response = session.get(url=link, headers=headers, proxies=proxies)
+        time.sleep(3)
         soup = BeautifulSoup(response.text, "lxml")
+        print(soup)
         # истец
-        plaintiff = soup.find_all("td", class_="plaintiffs")[1].find("a")
+        plaintiff = soup.find_all("td", class_="plaintiffs")[1]
         if plaintiff != None:
-            PLAINTIFFS.append(plaintiff.text.rstrip().lstrip())
+            new_plaintiff = plaintiff.find("a")
+            if new_plaintiff != None:
+                PLAINTIFFS.append(plaintiff.text.rstrip().lstrip())
+            else:
+                PLAINTIFFS.append(f"-")
         else:
             PLAINTIFFS.append(f"-")
         # ответчик
-        defendant = soup.find_all("td", class_="defendants")[1].find("a")
+        defendant = soup.find_all("td", class_="defendants")[1]
         if defendant != None:
-            DEFENDANTS.append(defendant.text.rstrip().lstrip())
+            new_defendant = defendant.find("a")
+            if new_defendant != None:
+                DEFENDANTS.append(defendant.text.rstrip().lstrip())
+            else:
+                DEFENDANTS.append(f"-")
         else:
             DEFENDANTS.append(f"-")
         # третьи лица
