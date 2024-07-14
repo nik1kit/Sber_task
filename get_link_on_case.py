8c10ac727846817e48df41b0c809f0a8575abd9c
nik1kit <49343348+nik1kit@users.noreply.github.com>
2024-07-14 14:20:05 +0300

    Update get_link_on_case.py

--- get_link_on_case.py
+++ get_link_on_case.py
@@ -5,16 +5,15 @@ from get_number_of_case import get_number_of_case
 from get_date_of_case import get_date
 
 
-def scrap_inf(html_code):
+def scrap_inf(html_code, session):
     links_in_main_case = html_code.find_all("a", class_="num_case")
     links_case = []
 
     for item in links_in_main_case:
         case_url = item.get("href")
-        links_case.append(case_url)  # массив ссылок на дела
-
+        links_case.append(case_url)  
     get_courts(html_code)
     get_inn(html_code)
     get_date(html_code)
     get_number_of_case(html_code)
-    link_processing(links_case)
+    link_processing(links_case, session)
